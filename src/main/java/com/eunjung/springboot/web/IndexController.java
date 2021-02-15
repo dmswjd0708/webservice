3fe47e0a8e33dadbfd72b63c032159723190ffe6
Eunjung <dms5wjd3@gmail.com>
2021-02-15 16:43:27 +0900

    스프링 시큐리티를 이용한 구글/네이버 로그인 연동

--- src/main/java/com/eunjung/springboot/web/IndexController.java
+++ src/main/java/com/eunjung/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.eunjung.springboot.web;
 
+import com.eunjung.springboot.config.auth.LoginUser;
+import com.eunjung.springboot.config.auth.dto.SessionUser;
 import com.eunjung.springboot.service.posts.PostsService;
 import com.eunjung.springboot.web.dto.PostsResponseDto;
 import lombok.Getter;
@@ -9,15 +11,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
